75161c94605199e28ce676a35ccaf87ff9cb468e
Felipe Puentes <feliipepuentes@gmail.com>
2019-05-22 08:17:13 -0500

    First problem solved

---
+++ Problem 1 - Multiples of 3 and 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Problem_1___Multiples_of_3_and_5
+{
+    class Multiples
+    {
+        static void Main(string[] args)
+        {
+
+            int maxNumber = 1000;
+            int sumOfMultiples = 0;
+            for(int i=1; i< maxNumber; i++)
+            {
+                if(i%3==0 || i%5==0)
+                {
+                    sumOfMultiples += i;
+                    Console.WriteLine("New Multiple: " +  i);
+                }
+            }
+
+            Console.WriteLine("The sum of the multiples of 3 or 5 is: " + sumOfMultiples);
+        }
+    }
+}
